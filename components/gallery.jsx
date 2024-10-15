137b294cc1768937e4355be0745ec2dacd5dff0d
Chikaysci <kjabellar@gmail.com>
2024-10-15 13:30:20 +0800

    updated files

--- components/gallery.jsx
+++ components/gallery.jsx
@@ -1,14 +1,7 @@
 import Image from 'next/image';
 
-export default function Gallery({ domain }) {
-  const imageUrls = [
-    "https://images.pexels.com/photos/27024507/pexels-photo-27024507.jpeg",
-    "https://images.pexels.com/photos/3952982/pexels-photo-3952982.jpeg",
-    "https://images.pexels.com/photos/7013719/pexels-photo-7013719.jpeg",
-    "https://images.pexels.com/photos/28492421/pexels-photo-28492421.jpeg",
-    "https://images.pexels.com/photos/5606164/pexels-photo-5606164.jpeg",
-    "https://images.pexels.com/photos/6695646/pexels-photo-6695646.jpeg"
-  ];
+export default function Gallery({ gallery }) {
+  
 
   return (
     <section className="py-5 text-white" id="portfolio">
@@ -16,10 +9,10 @@ export default function Gallery({ domain }) {
         <h2>My Works</h2>
         <p>Portfolio</p>
         <div className="gallery-container">
-          {imageUrls.map((url, index) => (
+        {gallery.map((url, index) => (
             <div key={index} className="gallery-item">
               <Image
-                src={url}
+                src={`https://www.profilesuite.com/uploads/gallery/`+url.filename}
                 alt={`Gallery Image ${index + 1}`}
                 width={300}
                 height={200}
--- components/hero.jsx
+++ components/hero.jsx
@@ -1,18 +1,20 @@
 import Image from 'next/image';
 
-export default function Hero({ domain }) {
+export default function Hero({ profile, links }) {
   return (
     <section 
       className="hero-section" 
-      style={{ backgroundImage: "url('https://images.pexels.com/photos/6695646/pexels-photo-6695646.jpeg')" }}
+      style={{ backgroundImage: "url(https://www.profilesuite.com/uploads/profile/"+profile.profile_image+")" }}
     >
       <div className="container container-padding">
         <div className="row align-items-center gy-6 gy-xl-0">
           <div className="col-md-12">
-            <h1 className="display-2 mb-4">Teetribe21</h1>
+            <h1 className="display-2 mb-4">{profile.name}</h1>
             <p className="lead">
-              At Teetribe21, we believe that T-shirts, mugs, and stickers can spark conversations or even movements. We create products that celebrate your unique identity and passions. For that conversation-starter merch that makes you say &quot;I Want One,&quot; visit: 
-              <a href="https://teetribe21.com" target="_blank" rel="noopener noreferrer"> teetribe21.com</a>
+              {profile.slogan}<br />
+              {links && links.length > 0 &&
+                 <a href={links[0].link} target="_blank" rel="noopener noreferrer">{links[0].title}</a>
+                }
             </p>
           </div>
         </div>
